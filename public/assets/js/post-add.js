f3289db5fe45dc9030061c8c193f6dd541315ca7
fmhooxx <1321058503@qq.com>
2019-08-14 14:56:28 +0800

    写文章功能第一次提交

---
+++ public/assets/js/post-add.js
@@ -0,0 +1,33 @@
+// 向服务器端发送请求 获取文章分类数据
+$.ajax({
+	url: '/categories',
+  type: 'get',
+  success:function (data) {
+    var html = template('categoryTpl', {result: data});
+    $('#category').html(html);
+  }
+});
+
+// 当管理员选择文件的时候 触发事件
+$('#feature').on('change', function () {
+  // 获取选中的文件
+  var file = this.files[0];
+  // 创建 formData 对象 实现二进制上传文件
+  var formData = new FormData();
+  // 将管理员选择的文件追加到 formData 中
+  formData.append('cover', file);
+  // 向服务器端发送请求 实现封面图片的上传
+  $.ajax({
+    type: 'post',
+		url: '/upload',
+		data: formData,
+		// 告诉$.ajax方法不要处理data属性对应的参数
+		processData: false,
+		// 告诉$.ajax方法不要设置参数类型
+		contentType: false,
+    success: function (data) {
+      console.log(data);
+      $('#thumbnail').val(data[0].cover);
+    }
+  })
+})
